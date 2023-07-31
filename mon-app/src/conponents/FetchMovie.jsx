1a006c13bb0fe9b9c8271ee10d40d781b45501d5
Théo Couvelard <mrcouvelard@gmail.com>
2023-07-31 12:25:38 +0200

    refactor code and actor known for

--- mon-app/src/conponents/FetchMovie.jsx
+++ mon-app/src/conponents/FetchMovie.jsx
@@ -14,21 +14,6 @@ const FetchMovie = () => {
     const split = search.split(' ');
     setSplited(split);
 
-    for (let i = 0; i < results.length; i++) {
-      // console.log(results[i]);
-      if (search === results[i].name || search === results[i].title) {
-        console.log(results[i].name ? results[i].name : results[i].title);
-        break;
-      }
-      // if(results[i].known_for){
-      //     for (let y = 0; y < results[i].known_for.length; y++) {
-      //         if (search === results[i].known_for[y].title) {
-      //             console.log(results[i].name);
-      //             // break
-      //         }
-      //     }
-      // }
-    }
   };
 
   useEffect(() => {
@@ -58,26 +43,35 @@ const FetchMovie = () => {
     fetchData();
   }, [splited, link]);
 
-  useEffect(() => {
-    // This useEffect will be triggered whenever 'results' changes
-    console.log(results);
-  }, [results]);
+  useEffect(() => {}, [results]);
 
   return (
     <>
-    <div className='box'>
-      <input onChange={handleSubmit} type='text' className='input' id='search' name='search' />
+      <div className='box'>
+        <input onChange={handleSubmit} type='text' className='input' id='search' name='search' />
       </div>
-      
+
       <div className='movieContainer'>
         {results.map((item, index) => {
 
-          return <div key={index}>
-          <MovieCard src={item.poster_path ? `https://image.tmdb.org/t/p/w500/${item.poster_path}` : ''} title={item.title} description={item.overview}/>
-          </div>
+
+          if (item.media_type != 'person' && item.media_type != 'tv') {
+            return <div key={index}>
+              <MovieCard src={item.poster_path ? `https://image.tmdb.org/t/p/w500/${item.poster_path}` : ''} title={item.title} description={item.overview} />
+            </div>
+          }
+
+          if(item.known_for){
+            return item.known_for.map((films, idx) => {
+              return <div key={idx}>
+                <MovieCard src={films.poster_path ? `https://image.tmdb.org/t/p/w500/${films.poster_path}` : ''} title={films.title ? films.title : films.name} description={films.overview} />
+              </div>
+            })
+          }
+
         })}
       </div>
-      </>
+    </>
 
   );
 };
--- mon-app/src/conponents/MovieCard.jsx
+++ mon-app/src/conponents/MovieCard.jsx
@@ -9,7 +9,7 @@ const MovieCard = (props) => {
   
   return (
     <div className="card" style={style} key={props.key}>
-      <img src={props.src} className="card-img-top" alt="" />
+      <img src={props.src} className="card-img-top" alt="" style={{height: '400px'}}/>
         <div className="card-body overflow-auto p-2" style={{height: '200px'}}>
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.description}</p>
